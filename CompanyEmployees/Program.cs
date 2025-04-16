e2903942778bd201e14804ddb710d5b43578519e
Вадим <werty193842@gmail.com>
2025-04-16 21:40:04 +0300

    Updated

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,46 @@
+using CompanyEmployees.Extensions;
+using Contracts;
+using LoggerService;
+using NLog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+LogManager.LoadConfiguration(Path.Combine(Directory.GetCurrentDirectory(), "nlog.config.xml"));  
+
+
+builder.Services.ConfigureIIS();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("CorsPolicy");
+
+app.Run();
